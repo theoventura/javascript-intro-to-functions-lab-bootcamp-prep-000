b7b8414b948195d3309381dbf7ce685257242ddb
Paolo Ventura <ventura.paolo@gmail.com>
2020-04-28 10:41:11 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+return string.toUpperCase()
+}
+
+function whisper(string) {
+return string.toLowerCase()
+}
+
+function logShout(string){
+console.log('HELLO');
+return string.toUpperCase()
+}
+
+function logWhisper(string){
+  console.log('hello');
+  return string.toLowerCase
+}
+
+function sayHiToGrandma(string){
+
+return whisper === console.log("I can't hear you!");
+
+return shout === console.log("YES INDEED");
+
+console.log("I love you, Grandma");
+return string.("I love you,too")
+
+}
